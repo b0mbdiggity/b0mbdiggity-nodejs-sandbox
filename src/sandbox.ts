76e878eaf3faa4257833cf31ed3cf7db85f168ef
b0mbdiggity <errcode.8@gmail.com>
2024-07-25 16:59:01 +0900

    git-crypt test

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,5 +1,11 @@
 const sandbox = () => {
   console.log();
+
+  type ReturnType<T> = T extends (...args: any[]) => infer R ? R : any;
+
+  type Func = () => number;
+
+  type FuncReturnType = ReturnType<Func>; // number
 };
 
 sandbox();
